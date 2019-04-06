2b52a04755b69fbdf5bb010d51161205e139f0ce
Prakhar Dev Gupta <39290396+prakhardg@users.noreply.github.com>
2019-04-06 08:45:20 +0530

    Update Merge 2 sorted linked list;.cpp

--- Linked List/Merge 2 sorted linked list;.cpp
+++ Linked List/Merge 2 sorted linked list;.cpp
@@ -1,23 +1,39 @@
-ListNode* Solution::mergeTwoLists(ListNode* a, ListNode* b) {
-    if(!a&& !b) return NULL;
-    if(a && !b)return a;
-    if(!a&&b) return b;
-      ListNode* result = NULL;
- 
-  
-  if (a->val <= b->val) 
-  {
-     result = a;
-     result->next = mergeTwoLists(a->next, b);
-  }
-  else
-  {
-     result = b;
-     result->next = mergeTwoLists(a, b->next);
-  }
-  return(result);
 
-}
 
 // Merge two sorted linked lists
 // Function only;
+
+Node * merge(Node * h1, Node * h2){
+    if(!h1 && !h2) return NULL;
+    if(h1 && ! h2) return h1;
+    if(h2 && ! h1) return h2;
+    Node * start = NULL, *end;
+    
+    start = h1->val < h2->val ? h1: h2;
+        if(start==h1){
+        h1  = h1->next;
+    }
+    else h2 = h2->next;
+    
+    while(h1 && h2){
+        if(h1->val <= h2->val){
+            end->next = h1;
+            end = h1;
+            h1 = h1->next;
+        }
+        else{
+        end->next  = h2;
+            end = h2;
+            h2 = h2->next;
+        }
+        if(h1) end->next = h1;
+        else if (h2) end->next = h2;
+        
+        
+        return start;
+
+    
+    }
+        
+
+}
